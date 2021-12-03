32fb8175f933e1495e460e47641e6d551a982866
modz hussein <mohamed.a.hussein.elsayed@gmail.com>
2021-12-04 01:55:07 +0200

    supporting cookies

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,34 @@ import './App.scss';
 import cryingWeed from './assets/videos/cryingWeed.mp4';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getCount = () =>{
+    let count_value = 0
+    let cookies = document.cookie.split(';')
+    for( let i =0;i<cookies.length; i++)
+    {
+      const [key,value] = cookies[i].split('=');
+      if (key === 'count')
+      {
+        count_value = parseInt(value)
+      }
+    }
+    return count_value;
+  }
+
+  const [count, setCount] = useState(getCount());
+
   const handleSmoking = (e:any) => {
     let video = document.getElementById('vid') as HTMLVideoElement
     video.currentTime = 0;
+    let date = new Date();
+    date.setTime(date.getTime() + (1000*24*60*60*1000));
     setCount(count+1)
+    document.cookie = `count=${count+1};expires=${date.toUTCString()};path=/`
     video.play();
   }
+
+
   return (
     <div className="home-container">
       <div className="counter">
